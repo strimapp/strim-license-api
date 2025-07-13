c2a1902ef2f3985abd62ff86265d3b9060b361fe
strimapp <kontaksaham101@gmail.com>
2025-07-13 23:49:09 +0700

    Update db.js

--- strim-license-api/db.js
+++ strim-license-api/db.js
@@ -1,15 +1,24 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
-let db;
+const options = {};
 
-async function connectDB() {
-  if (!db) {
-    await client.connect();
-    db = client.db("strim"); // Nama database
-  }
-  return db;
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('❌ Please add your Mongo URI to .env');
 }
 
-module.exports = connectDB;
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
